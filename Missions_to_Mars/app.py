a4cf4aa74088fca586c58235fb89f045dcd81b37
Logan Bonsignore <Loganbon14@gmail.com>
2020-09-13 20:16:24 -0600

    display edits

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template
 import scrape_mars
 import pymongo
-import random
+from random import randint
 from numpy import arange
 
 app = Flask(__name__)
@@ -12,9 +12,12 @@ db = client.mars_db.mission_to_mars
 def mars_data():
     scrape_mars.scrape()                      
     mars_data = db.find().sort([("_id", -1)]).limit(1)[0]
-                  
-    return render_template("index.html", news_titles=mars_data["news_titles"][0], \
-        news_paragraphs=mars_data["news_paragraphs"][0], \
+    rand_int = randint(0,len(mars_data["news_titles"]))
+    news = mars_data["news_titles"][rand_int]
+    paragraphs = mars_data["news_paragraphs"][rand_int]
+
+    return render_template("index.html", news_titles=news, \
+        news_paragraphs=paragraphs, \
         featured_image_url=mars_data["featured_image_url"], \
         table=mars_data["mars_facts_table_html"], \
         hemisphere_list=mars_data["hemisphere_list"] 
